e5d5fc47c0bfda2b9f565ec1c1efdd4b5ce2e530
Alan Dyeison <159740301+Alan22dye@users.noreply.github.com>
2024-11-06 16:30:50 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Profile from "./components/Profile"
+
+export default function App(){
+  return(
+    <body>
+      <Profile img={'./public/al.jpg'} name={'Alan Dyeison'} bio={'Técnico em desenvolvimento de sistemas 2/3'} phone={'+ 55 (85) 89840048'} email={'alvesalan8134@gmail.com'} githubUrl={'https://github.com/Alan22dye'} InstagramUrl={'https://www.instagram.com/_alan138'}></Profile>
+    </body> 
+  )
+}
