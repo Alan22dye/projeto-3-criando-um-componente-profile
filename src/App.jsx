ff5a4c4fb7a83afa35fdbecea348f799a67b0958
Alan Dyeison <159740301+Alan22dye@users.noreply.github.com>
2024-11-06 21:40:06 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Profile from "./components/Profile"
 export default function App(){
   return(
     <body>
-      <Profile img={'./public/al.jpg'} name={'Alan Dyeison'} bio={'Técnico em desenvolvimento de sistemas 2/3'} phone={'+ 55 (85) 89840048'} email={'alvesalan8134@gmail.com'} githubUrl={'https://github.com/Alan22dye'} InstagramUrl={'https://www.instagram.com/_alan138'}></Profile>
+      <Profile img={'./al.jpg'} name={'Alan Dyeison'} bio={'Técnico em desenvolvimento de sistemas 2/3'} phone={'+ 55 (85) 89840048'} email={'alvesalan8134@gmail.com'} githubUrl={'https://github.com/Alan22dye'} InstagramUrl={'https://www.instagram.com/_alan138'}></Profile>
     </body> 
   )
 }
